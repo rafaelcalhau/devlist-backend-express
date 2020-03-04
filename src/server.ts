ad022e02ab10a4aff248770f78d3f20a8733aff2
Rafael Calhau <rafael@calhau.me>
2020-03-04 00:12:04 -0300

    Added dotenv and created the example env file

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv'
 import app from './app'
 
-app.listen(3000, () => console.log('The aplication has been started.'))
+dotenv.config()
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log('The aplication has been started on PORT %s.', PORT))
